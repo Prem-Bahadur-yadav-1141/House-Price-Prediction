4308b92ac237b33f8743428ec4ab3da967105900
Prem-Bahadur-yadav-1141 <py272988@gmail.com>
2025-12-29 10:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
+st.image('https://www.bing.com/th/id/OGC.a0d928b00ef43581460c12faf9c1ff22?o=7&pid=1.7&rm=3&rurl=https%3a%2f%2fmedia0.giphy.com%2fmedia%2fKeEPTWaZc96RKqJnGs%2fgiphy.gif&ehk=GpF%2fTLp10K2s3fUwqgsh6HWZvNc8iGKhHmys6%2fD2nVc%3d')
 
 df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
@@ -24,3 +24,4 @@ for i in final_X:
   all_value.append(result)
 
 st.write(all_value)
+
